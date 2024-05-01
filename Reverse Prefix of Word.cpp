5fe826cfe5f356b5e03a96ba1735b8178a75d050
Sayed-Nahid <nahid15-3849@diu.edu.bd>
2024-05-01 11:35:18 +0600

    Adding LeetCode Template

--- Reverse Prefix of Word.cpp
+++ Reverse Prefix of Word.cpp
@@ -1,3 +1,9 @@
+//LeetCode UserName: SayedNahid
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//Problem-2000: Reverse Prefix of Word
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
@@ -9,3 +15,12 @@ public:
         }
     }
 };
+
+
+int main(){
+    Solution test;
+    cout << test.reversePrefix("abcdefd", 'd') << '\n';
+    cout << test.reversePrefix("xyxzxe", 'z') << '\n';
+    cout << test.reversePrefix("abcd", 'z') << '\n';
+    return 0;
+}
---
+++ leetcodeTemplate.cpp
@@ -0,0 +1,15 @@
+//LeetCode UserName: SayedNahid
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//Problem-
+
+
+int main(){
+    Solution test;
+    cout << test.() << '\n';
+    cout << test.() << '\n';
+    cout << test.() << '\n';
+    return 0;
+}
