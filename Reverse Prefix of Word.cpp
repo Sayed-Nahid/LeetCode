f1e8ee9f9f046bd6e95b8b798319dfab5a0ec8bd
Sayed-Nahid <nahid15-3849@diu.edu.bd>
2024-05-01 13:01:19 +0600

    new idea

--- Reverse Prefix of Word.cpp
+++ Reverse Prefix of Word.cpp
@@ -1,21 +1,16 @@
-//LeetCode UserName: SayedNahid
-
 #include<bits/stdc++.h>
 using namespace std;
 
+//LeetCode UserName: SayedNahid
 //Problem-2000: Reverse Prefix of Word
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int pos=word.find(ch), len=word.length()-1;
-        if(pos==-1) return word;
-        else{
-            reverse(word.begin(), word.begin()+pos+1);
-            return word;
-        }
+        int pos=word.find(ch);
+        reverse(word.begin(), word.begin()+pos+1);
+        return word;
     }
 };
-
 
 int main(){
     Solution test;
